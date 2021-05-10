9d19234a2859a17709645d705360d916c336fe69
HilFing-Real <indradip.paul@outlook.com>
2021-05-10 21:36:32 +0530

    Adding setup

---
+++ setup-private/mycog.py
@@ -0,0 +1,9 @@
+from redbot.core import commands
+
+class MyCog(commands.Cog):
+    """My custom cog"""
+
+    @commands.command()
+    async def setup(self,ctx)  
+      embed=discord.Embed(title="Setup", description="How to setup MagicBot exclusively for your server", color=0xFF5733)
+      await ctx.send(embed=embed)
